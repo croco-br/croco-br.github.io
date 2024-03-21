f450468044c427ce2b543cd8c51e44dcf7b2f4e5
Adriano Croco <adriano.daniel@soudaki.com>
2024-03-21 13:38:58 -0300

    fix opening

--- convert.sh
+++ convert.sh
@@ -7,10 +7,10 @@ if ! command -v convert &> /dev/null; then
 fi
 
 # Replace 'input_image.jpg' and 'output_image.png' with your file paths
-input_image="/home/croco/source/croco-br.github.io/IA/blue-logo-1024.jpeg"
-output_image="output_image.png"
+input_image="/home/croco/source/croco-br.github.io/IA/black-logo-1024.jpeg"
+output_image="introduction-visual.png"
 
 # Resize the image to 30x30
-convert "$input_image" -resize 30x30\! "$output_image"
+convert "$input_image" -resize 511x496\! "$output_image"
 
 echo "Image resized successfully!"
